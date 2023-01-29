af63bc960c7ae867568097f517b031d15943f45f
patrykskota <patryk.skota@interia.pl>
2023-01-29 22:33:00 +0100

    PRACA DOMOWA (IF, ELSE)

--- WyzwApp/WyzwApp/Program.cs
+++ WyzwApp/WyzwApp/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Puk, puk");
+﻿string name = "Piotr";
+var gender = "Mężczyzna";
+int age = 17;
+
+// == równe
+// != róznia sie
+// && TO I TO czy spełnia
+// || TO LUB TO czy spełnia
+// ! negacja
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta, poniżej 30lat");
+}
+else if (gender != "Kobieta" && age <= 17)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Inny człowiek");
+}
+
+
+
